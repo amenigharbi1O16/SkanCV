5b3cf883792711069717c13259cb44f9341cb6b1
Ameni Gharbi <amenigharbi1016@gmail.com>
2026-08-13 11:03:09 +0100

    feat(fastapi): bind FastApiClientInterface via FASTAPI_MODE config

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use App\Services\FastApi\FakeFastApiClient;
+use App\Services\FastApi\FastApiClientInterface;
+use App\Services\FastApi\RealFastApiClient;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +14,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(FastApiClientInterface::class, function () {
+            return match (config('services.fastapi.mode', 'fake')) {
+                'real' => new RealFastApiClient(config('services.fastapi.url')),
+                default => new FakeFastApiClient(),
+            };
+        });
     }
 
     /**
